261deeea8486f6af92efb6f4f3bb7f021bab72f0
PhadatareSumit <psumit5209@gmail.com>
2022-12-08 19:39:27 +0530

    Create variable assignment
    Create variable assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+var myFullName; 
+myFullName = ""
+console.log(myFullName)
+
+var myFullName; 
+myFullName = "SUMIT PHADATARE"
+console.log(myFullName)
+
+var myFullName; 
+myFullName = "SUMIT DATTATRAYA PHADATARE"
+console.log(myFullName)
+
+var myLove;
+myLove = "I LOVE ONLY JS"
+console.log(myLove)
+
+var firstName ="SUMIT";
+var lastName = "PHADATARE";
+var age = "27";
+var address = "At.Post-Bopgaon, Saswad, tehsil - Purandar, Dist- Pune";
+var pin_Codde = "412 301";
+var gender = "Male";
+var is_married = "NO";
+console.log(firstName, lastName, age, address, pin_Codde, gender, is_married)
