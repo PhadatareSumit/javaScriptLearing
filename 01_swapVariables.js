84f3d7f5906f8fb244569d3f8ea31801e1da263e
PhadatareSumit <psumit5209@gmail.com>
2022-12-16 20:09:26 +0530

    01_swapVariable Assignment
    01_swapVariable Assignment

---
+++ 01_swapVariables.js
@@ -0,0 +1,25 @@
+console.log("===================================Assignment 1.1=================================");
+
+var sweety ="sweety";
+var cutie = "cutie";
+console.log("============================before swap========================================");
+console.log(sweety, cutie);
+var temp = "sweety";
+var sweety = "cutie";
+var cutie = "sweety";
+console.log("===================================After swap=================================");
+console.log(sweety, cutie);
+
+console.log("===================================Assignment 1.2=================================");
+
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("============================before swap========================================");
+console.log(num1, num2, num3);
+var num1 = num2;
+var num2 = num3;
+var num3 = num1;
+console.log("===================================After swap=================================");
+console.log(num1, num2, num3);
