c42480b95515d23d48d3a859c5cc0f3aa4f05633
PhadatareSumit <psumit5209@gmail.com>
2023-01-16 14:57:21 +0530

    for loop
    index - point 5 assignment

---
+++ for_Loop_Assignment.js
@@ -0,0 +1,32 @@
+console.log(`--------------------Loop Assignment-------------------`);
+
+
+console.log(`--------------------* Point - 1 *-------------------`);
+for (let index = 5; index <=15; index++) {
+console.log(index);    
+}
+
+console.log(`--------------------* Point - 2 *-------------------`);
+for (let index = 50; index >=40; index--) {
+console.log(index);    
+}
+console.log(`--------------------* Point - 3 *-------------------`);
+for (let index = 1; index <=29; index=index+2) {
+console.log(index);
+}
+console.log(`--------------------* Point - 4 *-------------------`);
+for (let index = 0; index <=18; index=index+2) {
+console.log(index);
+}
+console.log(`--------------------* Point - 5 *-------------------`);
+for (let index = 5; index <=50; index=index+5) {
+console.log(index);
+}
+console.log(`--------------------* Point - 6 *-------------------`);
+for (let index = 10; index <=100; index=index+10) {
+console.log(index);
+}
+console.log(`--------------------* Point - 7 *-------------------`);
+for (let index = 100; index >=10; index=index-10) {
+console.log(index);
+}
