98c5d2ecda1fe3c4d6eb3abc179f1175ae9bb13f
PhadatareSumit <psumit5209@gmail.com>
2023-01-16 14:32:10 +0530

    compare assignment

---
+++ 04_compareAssignment.js
@@ -0,0 +1,20 @@
+var zero = 0 ==''
+console.log(`Result of (0=='') is = ${zero}`);
+
+var num = 0 =='0'
+console.log(`Result of (0 =='0') is = ${num}`);
+
+var one = 0 == false
+console.log(`Result of (0 == false) is = ${one}`);
+
+var two = null ==undefined
+console.log(`Result of (null== undefined) is = ${two}`);
+
+var three = 1 ==[1]
+console.log(`Result of (1 =[1]) is = ${three}`);
+
+var four = 1 == true
+console.log(`Result of (1 = true) is = ${four}`);
+
+var five = 1 =='1'
+console.log(`Result of (1 =[1]) is = ${five}`);
