08d8f300797b5e5df1593e773c2fc46bea4aea6e
PhadatareSumit <psumit5209@gmail.com>
2023-01-16 14:56:25 +0530

    string template

---
+++ string_Template_Assignment.js
@@ -0,0 +1,21 @@
+// var myDreamComapnyName= "PHADATARE FLORICULTURE FARM"
+// console.log(`My dream company name is : ${myDreamComapnyName}`);
+
+// OR//
+console.log(`(1.) My dream company name is "PHADATARE FLORICULTURE FARM" `);
+
+console.log(`----------------------------------------------------- `);
+
+var myhobbies1 = "Explore new places by riding Bike";
+var myhobbies2 = "Do visit Floriculture nurseries";
+var myhobbies3 = "Basketball";
+console.log("(2.)my hobbie are", myhobbies1, myhobbies2, myhobbies3);
+
+console.log(`----------------------------------------------------- `);
+
+console.log(`(3.)concat using string template------------------------------------- `);
+
+
+var stringTemplateAssignment = "Explore new places by riding Bike, Do visit Floriculture nurseries, Basketball"
+console.log("My hobbies using contat-",stringTemplateAssignment);
+
